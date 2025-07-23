f42e84c9c3a7a0bd47cf1e907ec371dc60d2212f
shoy77 <shoy77@gmail.com>
2025-07-24 00:00:42 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,21 @@
-const CACHE_NAME = 'bharath-radio-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png'
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
   );
-  self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
