f7bbb348bdd09742a72f0c605b814a2461929548
shoy77 <shoy77@gmail.com>
2025-08-01 00:22:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/icon-720.png'
 ];
 
 self.addEventListener('install', event => {
